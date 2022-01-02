cc54531e3e91f52345f718358f79be23735ae732
Scott Hoecker <hoeckers@gmail.com>
2022-01-02 12:55:47 -0600

    Study study study

--- code_examples.rb
+++ code_examples.rb
@@ -243,30 +243,366 @@ trumpet = Trumpet.new('C')
 p Instrument.instruments_produced # => 1
 
 # Variable Scope with Inheritance
+
+# Instance Variables with inheritance
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Lawyer < Person
+  def introduce
+    puts "Hi! I'm a lawyer, and my name is #{@name}" # @name accessible to subclass
+  end
+end
+
+class Paramedic < Person
+  def initialize(name); end
+
+  def introduce
+    puts "Hi! I'm a paramedic, and my name is #{@name}"
+  end
+end
+
+bob = Lawyer.new('Bob')
+bob.introduce # => "Hi! I'm a lawyer, and my name is Bob"
+jim = Paramedic.new('Jim')
+jim.introduce # => "Hi! I'm a paramedic, and my name is"
+
+# Class variables with inheritance
+class Instrument
+  @@instruments_produced = 0
+
+  def initialize
+    @@instruments_produced += 1
+  end
+
+  def self.instruments_produced
+    @@instruments_produced
+  end
+end
+
+class Trumpet < Instrument
+  @@instruments_produced = 2
+end
+
+puts Instrument.instruments_produced # => 0
+trumpet = Trumpet.new
+puts Instrument.instruments_produced # => 3
+
 # Polymorphism & Encapsulation
+
+
 # Polymorphism
+class Car
+  def move
+    puts "I'm driving!"
+  end
+end
+
+class Plane
+  def move
+    puts "I'm flying!"
+  end
+end
+
+car = Car.new
+plane = Plane.new
+car.move   # => "I'm driving!"
+plane.move # => "I'm flying!"
+
 # Polymorphism Through Inheritance
+class Instrument
+  def key
+  end
+end
+class Trumpet < Instrument
+  def key
+    puts "Bb"
+  end
+end
+
+class BassDrum < Instrument
+end
+
+trumpet = Trumpet.new
+drum = BassDrum.new
+trumpet.key # => "Bb"
+drum.key    # => No output, because drums aren't pitched
+
 # Polymorphism Through Duck Typing
+class Restaurant
+  attr_reader :guests, :ingredients
+
+  def service(employees)
+    employees.each do |employee|
+      employee.serve_food(self)
+    end
+  end
+end
+
+class Chef
+  def serve_food(restaurant)
+    make_food(restaurant.ingredients)
+  end
+
+  def make_food(ingredients)
+    # implementation
+  end
+end
+
+class Waiter
+  def serve_food(restaurant)
+    serve_guests(restaurant.guests)
+  end
+
+  def serve_guests(guests)
+    # implementation
+  end
+end
+
 # Encapsulation
+class BankAccount
+  attr_reader :balance
+
+  def initialize(initial_balance)
+    @balance = initial_balance
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def withdraw(amount)
+    self.balance -= amount if (balance - amount) >= 0
+  end
+
+  private
+
+  attr_writer :balance
+end
+
 # Modules
 # Mixin Modules
-# Namespacing
 # Module Methods
+module Packable
+  def pack
+    puts "I'm ready to go fly on a plane!"
+  end
+end
+
+class Trumpet    # => Trumpets can fly on a plane
+  include Packable
+end
+
+class GrandPiano # => Can't take a piano on a plane
+end
+
+# Namespacing
+module Instrument
+  class Trumpet
+    def self.play
+      puts "Toot toot!"
+    end
+  end
+
+  class BassDrum
+    def self.play
+      puts "Boom boom!"
+    end
+  end
+end
+
+Instrument::Trumpet.play  # => "Toot toot!"
+Instrument::BassDrum.play #=> "Boom boom!"
+
 # Self
 # Inside Instance Methods
-# Inside Class Methods
-# Inside Class Definitions
+class Person
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def introduce_self
+    puts "Hi, I'm #{self}"
+  end
+
+  def to_s
+    name
+  end
+end
+
+# self Inside Class Definitions
+# self Inside Class Methods
+class Person
+  def self.describe_self
+    puts "I'm a #{self}!"
+  end
+end
+
+Person.describe_self
+
 # Inside Mixin Modules
-# Outside Any Class
+module Packable
+  def pack?
+    puts "#{self.class}s can fly on planes!" # => self refers to calling object
+  end
+
+  def self.pack
+    puts "This is the #{self} module!"
+  end
+end
+
+class Trumpet    # => Trumpets can fly on a plane
+  include Packable
+end
+
+class GrandPiano # => Can't take a piano on a plane
+end
+
+trumpet = Trumpet.new
+trumpet.pack  # => "Trumpets can fly on planes!"
+Packable.pack # => "This is the Packable module!"
+
+# self Outside Any Class
+puts self # => main
+
 # Fake Operators and Equality
 # Equivalence
 # ==
+class Car
+  attr_reader :passengers
+
+  def initialize(passengers)
+    @passengers = passengers
+  end
+
+  def ==(other_car)
+    passengers == other_car.passengers
+  end
+end
+
+car1 = Car.new(2)
+car2 = Car.new(3)
+
+car1 == car2 # => false
+
 # equal? and object_id
+class Car
+end
+
+car1 = Car.new
+car2 = Car.new
+car1.object_id
+car2.object_id
+car1.equal?(car2) # => false
+
 # ===
+Class === String # => true
+String === Class # => false
+
 # eql?
+hash1 = { a: 1, b: 2 }
+hash2 = { a: 1, b: 2 }
+puts hash1.object_id
+puts hash2.object_id
+puts hash1.eql?(hash2) # => true
+
 # Fake Operators
 # Comparison Methods
+class BankAccount
+  def initialize(balance)
+    @balance = balance
+  end
+
+  def >(other_acct)
+    balance > other_acct.balance
+  end
+
+  def <(other_acct)
+    balance < other_acct.balance
+  end
+
+  private
+
+  attr_reader :balance
+end
+
+acct1 = BankAccount.new(50)
+acct2 = BankAccount.new(100)
+puts acct1 > acct2 # => false
+puts acct1 < acct2 # => true
+
 # Right and Left Shift
+class Car
+  attr_reader :passengers
+
+  def initialize
+    @passengers = []
+  end
+
+  def <<(passenger)
+    @passengers << passenger
+  end
+end
+
+car = Car.new
+car << "Bob"
+puts car.passengers # => Bob
+
 # Plus
+class CustomNumberClass
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def +(other_custom_number)
+    value + other_custom_number.value
+  end
+end
+
+one = CustomNumberClass.new(1)
+two = CustomNumberClass.new(2)
+
+puts(one + two) # => 3
+
 # Element Setters and Getters
+class Car
+  attr_reader :passengers
+
+  def initialize
+    @passengers = ["bob", "sara"]
+  end
+
+  def [](idx)
+    @passengers[idx]
+  end
+
+  def []=(idx, new_passenger)
+    @passengers[idx] = new_passenger
+  end
+end
+
+car = Car.new
+puts car[0] # => 'bob'
+car[0] = 'Scott'
+puts car[0] # => 'Scott'
+
+
 # Collaborator Objects
+class Band
+  def initialize
+    @drummer = Drummer.new("Ringo")
+  end
+end
+
+class Drummer
+  def initialize(name)
+    @name = name
+  end
+end
